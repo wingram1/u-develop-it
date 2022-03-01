7a7fec254f8438d5213be67d97d7b28de44aceba
wingram1 <waingram96@gmail.com>
2022-03-01 00:01:07 -0600

    database call set up

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello World'
-//     });
-// });
 
-// default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'j8T#01hGe%5',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 
@@ -22,7 +28,10 @@ app.use((req, res) => {
 
 
 
-
+// default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
